bc64e954d44ae0ec6ec739828d26a2cdf2e37ef5
Ilia0178 <barausov.ilia@yandex.ru>
2020-12-27 17:54:03 +0300

    Create pflfybt 1.py

---
+++ pflfybt 1.py
@@ -0,0 +1,11 @@
+print("Hello")
+user_name = input("Whot is your name?")
+user_year = int(input("When you was born?"))
+ print (2020-user_year)=user_year
+if user_year>18 :
+    print('посматри боевик,'user_name )
+   
+
+
+else :
+    print ('посмотри комедию, 'user_name )
