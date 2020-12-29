f818e5fc62e26a982983249c3e31984d5e027ed7
Ilia0178 <barausov.ilia@yandex.ru>
2020-12-29 14:14:43 +0300

    Create Home vorc number 2.2.py

---
+++ Home vorc number 2.2.py
@@ -0,0 +1,17 @@
+product=[]
+while a=False:
+    c=int(input('Вы хотите добавить продукт в список[1], удалить[2]посмотреть весь список[3],купить[4]'))
+    if c==1:
+        a=input('что нужно добавить? По 1 продукту')
+        produkt,append(a)
+    elif c==2:
+        print(produkt)
+        e=int(input('укажите элемент')
+        produkt.pop(e)
+    elif c==4:
+        not(a)
+    else:
+        print(produkt)
+print('спасибо за покупку')
+        
+    
