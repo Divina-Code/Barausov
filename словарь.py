0f49d96def796e37c1893d1fbb71af1f9534ea64
Ilia0178 <barausov.ilia@yandex.ru>
2020-12-30 17:01:01 +0300

    Create словарь.py

---
+++ словарь.py
@@ -0,0 +1,23 @@
+a={}
+import random
+
+while  :
+    f=input('добавить слово[1],играть[2]')
+    if f==1:
+        m=input('английское слово')
+        l=input('русское слово')
+        a[m]=l
+    if f==2:
+        e=1
+        while e==2 :
+            w=input('начать[1],закончить[2]')
+            if w==1
+                p=random.choice(list(a.keys()))
+                z=input('напишите перивод')
+                l=input(p )
+                if z==l :
+                    print('правильно')
+                else:
+                    print('не правильно')
+            else:
+                e=2
